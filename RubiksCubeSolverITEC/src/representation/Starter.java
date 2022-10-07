aec1777fd35cd0288728b8b078f1aff5ec9cbdd1
jasig <jasig@host.docker.internal>
2022-10-07 18:24:52 +0200

    asdasdas

--- RubiksCubeSolverITEC/src/representation/Starter.java
+++ RubiksCubeSolverITEC/src/representation/Starter.java
@@ -4,9 +4,10 @@ public class Starter {
 
 	public static void main(String[] args) {
 		
+		
 		int b = 1;
 		int a = 0b10010010111001;
-		b ^= 3;
+		a ^= 3;
  		System.out.println(Integer.toBinaryString(a));
 		
 		/*Wuerfel a = new Wuerfel();
