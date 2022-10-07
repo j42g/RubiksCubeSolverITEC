d0eb1cfc5b366c5504ed7d424b814e35719e5048
jasig <jasig@host.docker.internal>
2022-10-07 16:59:05 +0200

    Kommentare

--- RubiksCubeSolverITEC/src/representation/Starter.java
+++ RubiksCubeSolverITEC/src/representation/Starter.java
@@ -2,9 +2,59 @@ package representation;
 
 public class Starter {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public class Würfel {
+	    
+	    int[] faces;
 
+	    public boolean isSolved() {
+	        if (faces[0] == 0x00000000 &&
+	            faces[1] == 0x11111111 &&
+	            faces[2] == 0x22222222 &&
+	            faces[3] == 0x33333333 &&
+	            faces[4] == 0x44444444 &&
+	            faces[5] == 0x55555555) {
+	            return true;
+	        }
+	        return false;
+	    }
+	    
+	    public void printCube() {
+	        // convert to colors
+	        char[][] coloredCube = new char[3][6];
+	        // print white face
+	        System.out.println("\t" + faces[0] + " ");
+	        System.out.println("\t" + faces[0]);
+	        System.out.println("\t" + faces[0]);
+	    }
+	    
+	    public void printFace(int index) {
+	        for(int i = 0; i < 9; i++) {
+	            if(i < 3) {
+	                System.out.print(lookupColor(extractColor(faces[index], i)) + " ");
+	            } else if (i == 4) {
+	                System.out.println(lookupColor(extractColor(faces[index], i)));
+	            }
+	            if(i == 5) {
+	                
+	            }
+	        }
+	    }
+	    
+	    public char lookupColor(int code){
+	        switch(code) {
+	        case 0: return 'W';
+	        case 1: return 'B';
+	        case 2: return 'O';
+	        case 3: return 'G';
+	        case 4: return 'R';
+	        case 5: return 'Y';
+	        }
+	        return (Character) null;
+	    }
+	    
+	    public int extractColor(int face, int index) { 
+	        return (face >>> (index*4)) & (0xF);
+	    }
 	}
 
 }
