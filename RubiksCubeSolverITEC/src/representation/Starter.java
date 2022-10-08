dc9d598921518e3a4d9e543569deba78b13b2275
jasig <jasig@host.docker.internal>
2022-10-08 14:58:07 +0200

    typos

--- RubiksCubeSolverITEC/src/representation/Starter.java
+++ RubiksCubeSolverITEC/src/representation/Starter.java
@@ -6,12 +6,15 @@ public class Starter {
 			
 		Wuerfel a = new Wuerfel();
 		a.makeSolved();
-		a.dreheZug(0b1000);
-		/*a.veraendereEinzeln(0, 1, 5);
+		
+		a.veraendereEinzeln(0, 1, 5);
 		a.veraendereEinzeln(4, 1, 1);
 		a.veraendereEinzeln(2, 3, 5);
 		a.veraendereEinzeln(3, 4, 2);
-		a.veraendereEinzeln(1, 0, 0);*/
+		a.veraendereEinzeln(1, 0, 0);
+		
+		a.wuerfelAusgeben();
+		a.dreheZug(0b0000);
 		a.wuerfelAusgeben();
 		
 
