691e14711e22c080cca0714bb1bc73e600aac912
jasig <jasig@DESKTOP-GQ0A0C3.fritz.box>
2022-10-15 10:54:31 +0200

    solver gefixt

--- RubiksCubeSolverITEC/src/loeser/ZweiMalZwei.java
+++ RubiksCubeSolverITEC/src/loeser/ZweiMalZwei.java
@@ -11,8 +11,8 @@ public class ZweiMalZwei {
 	//private final int[] ersteEckeDaten = { 0x00000000, 0x01000001, 0x02000002, 0x03000003, 0x04040000, 0x00000000 };
 	//private final int[] ersteEckeMasken = { 0x0F0F0F0F, 0x0F00000F, 0x0F00000F, 0x0F00000F, 0x0F0F0000, 0x00000000 };
 	
-	private final int[] ersteSeiteDaten = { 0x00000000, 0x00000000, 0x00000000, 0x00000003, 0x00040000, 0x00000000 };
-	private final int[] ersteSeiteMasken = { 0x000F0000, 0x00000000, 0x00000000, 0x0000000F, 0x000F0000, 0x00000000 };
+	private final int[] DBLEckeDaten =  { 0x00000000, 0x00000100, 0x00020000, 0x00000000, 0x00000000, 0x05000000 };
+	private final int[] DBLEckeMasken = { 0x00000000, 0x00000F00, 0x000F0000, 0x00000000, 0x00000000, 0x0F000000 };
 	
 	private final int[] loeseDaten = { 0x00000000, 0x01010101, 0x02020202, 0x03030303, 0x04040404, 0x05050505 };
 	private final int[] loeseMasken = { 0x0F0F0F0F, 0x0F0F0F0F, 0x0F0F0F0F, 0x0F0F0F0F, 0x0F0F0F0F, 0x0F0F0F0F };
@@ -24,17 +24,21 @@ public class ZweiMalZwei {
 	}
 	
 	public void loese() {
-		/*IDDFS pattern = new IDDFS(this.w.getSeiten(), this.ersteEckeDaten, this.ersteEckeMasken);
-		w.dreheZugsequenz(pattern.start());
-		this.solveSequenz += Util.kodeZuegeZuNotation(pattern.start());
 		
-		this.w.wuerfelAusgeben();*/
+		// DBL-Ecke lösen
 		
-		IDDFS pattern = new IDDFS(this.w.getSeiten(), this.loeseDaten, this.loeseMasken, this.loeseZuege);
+		IDDFS pattern = new IDDFS(this.w.getSeiten(), this.DBLEckeDaten, this.DBLEckeMasken);
 		w.dreheZugsequenz(pattern.loese());
-		this.solveSequenz += Util.kodeZuegeZuNotation(pattern.loese());
+		//this.solveSequenz += Util.kodeZuegeZuNotation(pattern.loese());
 		
-		System.out.println("Gelöst mit: " + this.solveSequenz);
+		//w.wuerfelAusgeben();
+		
+		
+		pattern = new IDDFS(this.w.getSeiten(), this.loeseDaten, this.loeseMasken, this.loeseZuege);
+		w.dreheZugsequenz(pattern.loese());
+		//this.solveSequenz += Util.kodeZuegeZuNotation(pattern.loese());
+		
+		//System.out.println("Gelöst mit: " + this.solveSequenz);
 	}
 	
 }
