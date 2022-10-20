b457f38debf4dac83a00ce2086fcb587a2b8c001
jasig <jasig@host.docker.internal>
2022-10-20 23:03:08 +0200

    Input Handling

--- RubiksCubeSolverITEC/src/renderer/Input.java
+++ RubiksCubeSolverITEC/src/renderer/Input.java
@@ -5,15 +5,28 @@ import org.lwjgl.glfw.GLFWKeyCallback;
 
 public class Input {
 	
-	private boolean[] keys = new boolean[GLFW.GLFW_KEY_LAST];
-	
 	private GLFWKeyCallback keyboard;
 	
 	public Input() {
 		this.keyboard = new GLFWKeyCallback() {
 			@Override
 			public void invoke(long window, int key, int scancode, int action, int mods) {
-				keys[key] = (action != GLFW.GLFW_RELEASE);
+				
+
+				if(GLFW.GLFW_KEY_U == key && action == GLFW.GLFW_PRESS) {
+					System.out.println("U-Zug");
+				} else if(GLFW.GLFW_KEY_R == key && action == GLFW.GLFW_PRESS) {
+					System.out.println("R-Zug");
+				} else if(GLFW.GLFW_KEY_F == key && action == GLFW.GLFW_PRESS) {
+					System.out.println("F-Zug");
+				} else if(GLFW.GLFW_KEY_D == key && action == GLFW.GLFW_PRESS) {
+					System.out.println("D-Zug");
+				} else if(GLFW.GLFW_KEY_L == key && action == GLFW.GLFW_PRESS) {
+					System.out.println("L-Zug");
+				} else if(GLFW.GLFW_KEY_B == key && action == GLFW.GLFW_PRESS) {
+					System.out.println("B-Zug");
+				}
+				
 				
 			}
 		};
@@ -22,10 +35,11 @@ public class Input {
 	}
 	
 	
+	
 	public void destory() {
 		this.keyboard.free();
 	}
-
+	
 	public GLFWKeyCallback getKeyboardCallback() {
 		return keyboard;
 	}
