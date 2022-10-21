5a42dae3203831a067098006bb87f3c2d201ebe5
jasig <jasig@host.docker.internal>
2022-10-21 23:52:30 +0200

    stuff

---
+++ RubiksCubeSolverITEC/src/graphic/renderer/ScreenThread.java
@@ -0,0 +1,9 @@
+package graphic.renderer;
+
+public class ScreenThread extends Thread {
+	
+	public void run() {
+		Screen.anfangen();
+	}
+
+}
--- RubiksCubeSolverITEC/src/representation/Starter.java
+++ RubiksCubeSolverITEC/src/representation/Starter.java
@@ -1,13 +1,16 @@
 package representation;
 
 import loeser.*;
+import graphic.renderer.ScreenThread;
 import kociembaDarstellung.*;
 
 
 public class Starter {
 
 	public static void main(String[] args) {
-
+		
+		ScreenThread a = new ScreenThread();
+		a.start();
 		
 		/*CubieWuerfel cubie = new CubieWuerfel();
 		cubie.mul(Zuege.alleZuege[Zuege.F1]);
