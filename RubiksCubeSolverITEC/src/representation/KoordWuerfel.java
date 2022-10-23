fc8c251cbc731c1c48d90db1120a4131d974f0f3
jasig <jasig@host.docker.internal>
2022-10-23 18:35:25 +0200

    koordwuerfel angefangen

---
+++ RubiksCubeSolverITEC/src/representation/KoordWuerfel.java
@@ -0,0 +1,14 @@
+package representation;
+
+public class KoordWuerfel {
+	
+	private final static int SOLVED = 0; // auﬂer u_edges
+	private int twist;
+	private int flip;
+	private int slice_sorted;
+	private int u_edges;
+	private int d_edges;
+	private int corners;
+	private int ud_edges;
+	
+}
