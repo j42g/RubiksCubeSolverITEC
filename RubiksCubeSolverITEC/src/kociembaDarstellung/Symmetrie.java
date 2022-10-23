b3393d7c1779c63ec7708ec11efb1f358acbcbce
jasig <jasig@host.docker.internal>
2022-10-23 14:16:14 +0200

    symmetrie definitionen

---
+++ RubiksCubeSolverITEC/src/kociembaDarstellung/Symmetrie.java
@@ -0,0 +1,10 @@
+package kociembaDarstellung;
+
+public class Symmetrie {
+	
+	public static final int ROT_URF3 = 0;
+	public static final int ROT_F2 = 0;
+	public static final int ROT_U4 = 0;
+	public static final int MIRR_LR2 = 0;
+	
+}
