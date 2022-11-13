a713c13f18f9388396c95fd35ae562aaf76e9c7c
j42g <jasi.guenther@gmail.com>
2022-11-13 02:21:42 +0100

    sinnvollere Sichtbarkeit und Anordnung

--- RubiksCubeSolverITEC/src/loeser/CFOP.java
+++ RubiksCubeSolverITEC/src/loeser/CFOP.java
@@ -6,47 +6,38 @@ import representation.Zuege;
 
 public class CFOP extends Thread {
 
-	private final Wuerfel w;
-
-	/**
-	 * 0: kein Debug; 1: Würfel nach jeden Schritt ausgeben
-	 */
-	private final int debug;
-
-	private String solveSequenz;
-
-	private final int[] kreuzDaten = { 0x00000000, 0x10000000, 0x20000000, 0x30000000, 0x00400000, 0x00000000 };
-	private final int[] kreuzMaske = { 0xF0F0F0F0, 0xF0000000, 0xF0000000, 0xF0000000, 0x00F00000, 0x00000000 };
+	private static final int[] kreuzDaten = { 0x00000000, 0x10000000, 0x20000000, 0x30000000, 0x00400000, 0x00000000 };
+	private static final int[] kreuzMaske = { 0xF0F0F0F0, 0xF0000000, 0xF0000000, 0xF0000000, 0x00F00000, 0x00000000 };
 	// Orange Green F2L Corner
-	private final int[] F2LOG1Maske = { 0xFFF0F0F0, 0xF0000000, 0xF000000F, 0xFF000000, 0x00F00000, 0x00000000 };
-	private final int[] F2LOG1Daten = { 0x00000000, 0x10000000, 0x20000002, 0x33000000, 0x00400000, 0x00000000 };
+	private static final int[] F2LOG1Maske = { 0xFFF0F0F0, 0xF0000000, 0xF000000F, 0xFF000000, 0x00F00000, 0x00000000 };
+	private static final int[] F2LOG1Daten = { 0x00000000, 0x10000000, 0x20000002, 0x33000000, 0x00400000, 0x00000000 };
 	// Orange Green F2L Full
-	private final int[] F2LOG2Maske = { 0xFFF0F0F0, 0xF0000000, 0xF00000FF, 0xFFF00000, 0x00F00000, 0x00000000 };
-	private final int[] F2LOG2Daten = { 0x00000000, 0x10000000, 0x20000022, 0x33300000, 0x00400000, 0x00000000 };
+	private static final int[] F2LOG2Maske = { 0xFFF0F0F0, 0xF0000000, 0xF00000FF, 0xFFF00000, 0x00F00000, 0x00000000 };
+	private static final int[] F2LOG2Daten = { 0x00000000, 0x10000000, 0x20000022, 0x33300000, 0x00400000, 0x00000000 };
 	// Green Red F2L Corner
-	private final int[] F2LGR1Maske = { 0xFFFFF0F0, 0xF0000000, 0xF00000FF, 0xFFF0000F, 0x00FF0000, 0x00000000 };
-	private final int[] F2LGR1Daten = { 0x00000000, 0x10000000, 0x20000022, 0x33300003, 0x00440000, 0x00000000 };
+	private static final int[] F2LGR1Maske = { 0xFFFFF0F0, 0xF0000000, 0xF00000FF, 0xFFF0000F, 0x00FF0000, 0x00000000 };
+	private static final int[] F2LGR1Daten = { 0x00000000, 0x10000000, 0x20000022, 0x33300003, 0x00440000, 0x00000000 };
 	// Green Red F2L Full
-	private final int[] F2LGR2Maske = { 0xFFFFF0F0, 0xF0000000, 0xF00000FF, 0xFFF000FF, 0x00FFF000, 0x00000000 };
-	private final int[] F2LGR2Daten = { 0x00000000, 0x10000000, 0x20000022, 0x33300033, 0x00444000, 0x00000000 };
+	private static final int[] F2LGR2Maske = { 0xFFFFF0F0, 0xF0000000, 0xF00000FF, 0xFFF000FF, 0x00FFF000, 0x00000000 };
+	private static final int[] F2LGR2Daten = { 0x00000000, 0x10000000, 0x20000022, 0x33300033, 0x00444000, 0x00000000 };
 	// Red Blue F2L Corner
-	private final int[] F2LRB1Maske = { 0xFFFFFFF0, 0xFF000000, 0xF00000FF, 0xFFF000FF, 0x0FFFF000, 0x00000000 };
-	private final int[] F2LRB1Daten = { 0x00000000, 0x11000000, 0x20000022, 0x33300033, 0x04444000, 0x00000000 };
+	private static final int[] F2LRB1Maske = { 0xFFFFFFF0, 0xFF000000, 0xF00000FF, 0xFFF000FF, 0x0FFFF000, 0x00000000 };
+	private static final int[] F2LRB1Daten = { 0x00000000, 0x11000000, 0x20000022, 0x33300033, 0x04444000, 0x00000000 };
 	// Red Blue F2L Full
-	private final int[] F2LRB2Maske = { 0xFFFFFFF0, 0xFFF00000, 0xF00000FF, 0xFFF000FF, 0xFFFFF000, 0x00000000 };
-	private final int[] F2LRB2Daten = { 0x00000000, 0x11100000, 0x20000022, 0x33300033, 0x44444000, 0x00000000 };
+	private static final int[] F2LRB2Maske = { 0xFFFFFFF0, 0xFFF00000, 0xF00000FF, 0xFFF000FF, 0xFFFFF000, 0x00000000 };
+	private static final int[] F2LRB2Daten = { 0x00000000, 0x11100000, 0x20000022, 0x33300033, 0x44444000, 0x00000000 };
 	// Blue Orange Corner
-	private final int[] F2LBO1Maske = { 0xFFFFFFFF, 0xFFF0000F, 0xFF0000FF, 0xFFF000FF, 0xFFFFF000, 0x00000000 };
-	private final int[] F2LBO1Daten = { 0x00000000, 0x11100001, 0x22000022, 0x33300033, 0x44444000, 0x00000000 };
+	private static final int[] F2LBO1Maske = { 0xFFFFFFFF, 0xFFF0000F, 0xFF0000FF, 0xFFF000FF, 0xFFFFF000, 0x00000000 };
+	private static final int[] F2LBO1Daten = { 0x00000000, 0x11100001, 0x22000022, 0x33300033, 0x44444000, 0x00000000 };
 	// Blue Orange F2L
-	private final int[] F2LBO2Maske = { 0xFFFFFFFF, 0xFFF000FF, 0xFFF000FF, 0xFFF000FF, 0xFFFFF000, 0x00000000 };
-	private final int[] F2LBO2Daten = { 0x00000000, 0x11100011, 0x22200022, 0x33300033, 0x44444000, 0x00000000 };
+	private static final int[] F2LBO2Maske = { 0xFFFFFFFF, 0xFFF000FF, 0xFFF000FF, 0xFFF000FF, 0xFFFFF000, 0x00000000 };
+	private static final int[] F2LBO2Daten = { 0x00000000, 0x11100011, 0x22200022, 0x33300033, 0x44444000, 0x00000000 };
 
 
 	// OLL
-	private final int[] OLLMaske = {0xFFFFFFFF, 0xFFF000FF, 0xFFF000FF, 0xFFF000FF, 0xFFFFF000, 0xFFFFFFFF};
-	private final int[] OLLDaten = {0x00000000, 0x11100011, 0x22200022, 0x33300033, 0x44444000, 0x55555555};
-	private final int[][] OLLzuege = {
+	private static final int[] OLLMaske = {0xFFFFFFFF, 0xFFF000FF, 0xFFF000FF, 0xFFF000FF, 0xFFFFF000, 0xFFFFFFFF};
+	private static final int[] OLLDaten = {0x00000000, 0x11100011, 0x22200022, 0x33300033, 0x44444000, 0x55555555};
+	private static final int[][] OLLzuege = {
 			{Zuege.D1}, // drehen
 			{Zuege.D2},
 			{Zuege.D3},
@@ -65,9 +56,9 @@ public class CFOP extends Thread {
 	};
 
 	// PLL
-	private final int[] PLLMaske = {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF};
-	private final int[] PLLDaten = {0x00000000, 0x11111111, 0x22222222, 0x33333333, 0x44444444, 0x55555555};
-	private final int[][] PLLzuege = {
+	private static final int[] PLLMaske = {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF};
+	private static final int[] PLLDaten = {0x00000000, 0x11111111, 0x22222222, 0x33333333, 0x44444444, 0x55555555};
+	private static final int[][] PLLzuege = {
 			{Zuege.D1}, // drehen
 			{Zuege.D2},
 			{Zuege.D3},
@@ -79,7 +70,9 @@ public class CFOP extends Thread {
 			{14, 17, 13, 15, 12, 15, 14, 17, 12, 15, 12, 17, 12, 17, 14} // Z
 	};
 
-
+	private final Wuerfel w;
+	private final int debug;
+	private String solveSequenz;
 
 	public CFOP(Wuerfel _w, int _debug) {
 		this.w = _w;
@@ -97,68 +90,68 @@ public class CFOP extends Thread {
 
 	public void beginne() {
 		// Kreuz
-		IDDFS pattern = new IDDFS(this.w.getSeiten(), this.kreuzDaten, this.kreuzMaske);
+		IDDFS pattern = new IDDFS(this.w.getSeiten(), kreuzDaten, kreuzMaske);
 		w.dreheZugsequenz(pattern.loese());
 		this.solveSequenz += Zuege.lookupZugseq(pattern.loese());
 		if (this.debug == 1)
 			w.ausgeben();
 
-		pattern = new IDDFS(this.w.getSeiten(), this.F2LOG1Daten, this.F2LOG1Maske);
+		pattern = new IDDFS(this.w.getSeiten(), F2LOG1Daten, F2LOG1Maske);
 		w.dreheZugsequenz(pattern.loese());
 		this.solveSequenz += Zuege.lookupZugseq(pattern.loese());
 		if (this.debug == 1)
 			w.ausgeben();
 
-		pattern = new IDDFS(this.w.getSeiten(), this.F2LOG2Daten, this.F2LOG2Maske);
+		pattern = new IDDFS(this.w.getSeiten(), F2LOG2Daten, F2LOG2Maske);
 		w.dreheZugsequenz(pattern.loese());
 		this.solveSequenz += Zuege.lookupZugseq(pattern.loese());
 		if (this.debug == 1)
 			w.ausgeben();
 
-		pattern = new IDDFS(this.w.getSeiten(), this.F2LGR1Daten, this.F2LGR1Maske);
+		pattern = new IDDFS(this.w.getSeiten(), F2LGR1Daten, F2LGR1Maske);
 		w.dreheZugsequenz(pattern.loese());
 		this.solveSequenz += Zuege.lookupZugseq(pattern.loese());
 		if (this.debug == 1)
 			w.ausgeben();
 
-		pattern = new IDDFS(this.w.getSeiten(), this.F2LGR2Daten, this.F2LGR2Maske);
+		pattern = new IDDFS(this.w.getSeiten(), F2LGR2Daten, F2LGR2Maske);
 		w.dreheZugsequenz(pattern.loese());
 		this.solveSequenz += Zuege.lookupZugseq(pattern.loese());
 		if (this.debug == 1)
 			w.ausgeben();
 
-		pattern = new IDDFS(this.w.getSeiten(), this.F2LRB1Daten, this.F2LRB1Maske);
+		pattern = new IDDFS(this.w.getSeiten(), F2LRB1Daten, F2LRB1Maske);
 		w.dreheZugsequenz(pattern.loese());
 		this.solveSequenz += Zuege.lookupZugseq(pattern.loese());
 		if (this.debug == 1)
 			w.ausgeben();
 
-		pattern = new IDDFS(this.w.getSeiten(), this.F2LRB2Daten, this.F2LRB2Maske);
+		pattern = new IDDFS(this.w.getSeiten(), F2LRB2Daten, F2LRB2Maske);
 		w.dreheZugsequenz(pattern.loese());
 		this.solveSequenz += Zuege.lookupZugseq(pattern.loese());
 		if (this.debug == 1)
 			w.ausgeben();
 
-		pattern = new IDDFS(this.w.getSeiten(), this.F2LBO1Daten, this.F2LBO1Maske);
+		pattern = new IDDFS(this.w.getSeiten(), F2LBO1Daten, F2LBO1Maske);
 		w.dreheZugsequenz(pattern.loese());
 		this.solveSequenz += Zuege.lookupZugseq(pattern.loese());
 		if (this.debug == 1)
 			w.ausgeben();
 
-		pattern = new IDDFS(this.w.getSeiten(), this.F2LBO2Daten, this.F2LBO2Maske);
+		pattern = new IDDFS(this.w.getSeiten(), F2LBO2Daten, F2LBO2Maske);
 		w.dreheZugsequenz(pattern.loese());
 		this.solveSequenz += Zuege.lookupZugseq(pattern.loese());
 		if (this.debug == 1)
 			w.ausgeben();
 
 		// OLL
-		IDDFSSeqs oll = new IDDFSSeqs(this.w.getSeiten(), this.OLLDaten, this.OLLMaske, this.OLLzuege);
+		IDDFSSeqs oll = new IDDFSSeqs(this.w.getSeiten(), OLLDaten, OLLMaske, OLLzuege);
 		w.dreheZugsequenz(oll.loese());
 		this.solveSequenz += Zuege.lookupZugseq(oll.loese());
 		if (this.debug == 1) w.ausgeben();
 
 		// PLL
-		IDDFSSeqs pll = new IDDFSSeqs(this.w.getSeiten(), this.PLLDaten, this.PLLMaske, this.PLLzuege);
+		IDDFSSeqs pll = new IDDFSSeqs(this.w.getSeiten(), PLLDaten, PLLMaske, PLLzuege);
 		w.dreheZugsequenz(pll.loese());
 		this.solveSequenz += Zuege.lookupZugseq(pll.loese());
 		if (this.debug == 1) w.ausgeben();
